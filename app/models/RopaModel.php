a29b03414ff2e69f42acdcafb7d015bf242213cc
Thomas-Arnaiz <tarnaiz@alumnos.exa.unicen.edu.ar>
2026-06-29 16:24:01 -0300

    Update RopaModel.php

--- app/models/RopaModel.php
+++ app/models/RopaModel.php
@@ -1,35 +1,24 @@
 <?php
 require_once 'config/config.php';
 
-class RopaModel {
+class RopaModel
+{
     protected $db;
 
-    public function __construct() {
+    public function __construct()
+    {
         try {
-            $this->db = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8', DB_USER, DB_PASS);
+            $this->db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
             $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             die("Connection error: " . $e->getMessage());
         }
     }
 
-    public function getRopa($sort = 'ropa_id', $order = 'ASC') {
+    public function getRopa($sort='nombre', $order = 'ASC')
+    {
         try {
-            $allowedColumns = ['ropa_id', 'nombre', 'precio', 'talle_id', 'nombre_talle'];
-            
-            if (empty($sort) || !in_array($sort, $allowedColumns)) {
-                $sort = 'ropa_id';
-            }
-            if (empty($order) || !in_array(strtoupper($order), ['ASC', 'DESC'])) {
-                $order = 'ASC';
-            }
-
-            $sql = 'SELECT ropa.*, talles.nombre_talle 
-                    FROM ropa 
-                    JOIN talles ON ropa.talle_id = talles.talle_id
-                    ORDER BY ' . $sort . ' ' . $order;
-                    
-            $query = $this->db->prepare($sql);
+            $query = $this->db->prepare("SELECT*FROM ropa JOIN talles ON ropa.talle_id = talles.talle_id ORDER BY $sort $order");
             $query->execute();
             return $query->fetchAll(PDO::FETCH_OBJ);
         } catch (PDOException $e) {
@@ -37,14 +26,11 @@ class RopaModel {
         }
     }
 
-    public function getRopaById($id) {
+    public function getRopaById($id)
+    {
         try {
             $query = $this->db->prepare(
-                'SELECT ropa.*, talles.nombre_talle 
-                 FROM ropa 
-                 JOIN talles ON ropa.talle_id = talles.talle_id
-                 WHERE ropa.ropa_id = ?'
-            );
+                'SELECT*FROM ropa  JOIN talles ON ropa.talle_id = talles.talle_id WHERE ropa.ropa_id = ?' );
             $query->execute([$id]);
             return $query->fetch(PDO::FETCH_OBJ);
         } catch (PDOException $e) {
@@ -52,7 +38,8 @@ class RopaModel {
         }
     }
 
-    public function insertRopa($nombre, $precio, $talle_id) {
+    public function insertRopa($nombre, $precio, $talle_id)
+    {
         try {
             $query = $this->db->prepare(
                 'INSERT INTO ropa (nombre, precio, talle_id) 
@@ -65,7 +52,8 @@ class RopaModel {
         }
     }
 
-    public function updateRopa($id, $nombre, $precio, $talle_id) {
+    public function updateRopa($id, $nombre, $precio, $talle_id)
+    {
         try {
             $query = $this->db->prepare(
                 'UPDATE ropa 
